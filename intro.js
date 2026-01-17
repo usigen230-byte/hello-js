b9917192ce03ebbb1e88e47bf1c01f0aadad3d4e
Jeremiah Kessington Osinigwe <usigen230@gmail.com>
2026-01-17 09:10:11 +0100

    initial commit: Introduction of me

---
+++ intro.js
@@ -0,0 +1,7 @@
+const names = "Jeremiah Osinigwe Kessington";
+const age = "19 years old";
+const favourite_programming_lang = "python";
+
+console.log(names);
+console.log(age)
+console.log(favourite_programming_lang)
